1324582eb57b0d62ffc56a5d84f5653be0a63b62
giahungbk <giahung197bg@gmail.com>
2018-11-02 18:47:44 +0700

    update canvas

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-const yourDate = new Date("2015-11-02T02:10:26"),
-    music = ["dnty.mp3", "123.mp3", "nngdc.mp3"],
-    dNow = new Date();
-
 document.addEventListener("DOMContentLoaded", function () {
+    const yourDate = new Date("2015-11-02T02:10:26");
+    const music = ["dnty.mp3", "123.mp3", "nngdc.mp3"];
+    const dNow = new Date();
     let dateCount = document.querySelector('date');
     let wrapper = document.querySelector('.wrapper');
     let timeNow = document.querySelector('#today');
@@ -29,5 +28,83 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.querySelector('audio').setAttribute('src', `music/${music[Math.floor(Math.random()*music.length)]}`);
 
-    wrapper.insertAdjacentHTML("afterend",`<div class='mask'></div>`);
-});
+    wrapper.insertAdjacentHTML("afterend",
+        `<div class='mask'></div>
+        <canvas class="canvas"></canvas>`
+    );
+
+    // Canvas
+    let canvas = document.querySelector('.canvas');
+    let c = canvas.getContext('2d');
+    let mouse = {
+        x: undefined,
+        y: undefined
+    }
+
+    window.addEventListener('mousemove', function (event) {
+        mouse.x = event.clientX;
+        mouse.y = event.clientY;
+    });
+
+    //set size canvas
+    function setCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+    setCanvas();
+    window.addEventListener('resize', setCanvas);
+
+    function createX() {
+        return Math.floor(Math.random() * canvas.width);
+    }
+
+    function createY() {
+        return Math.floor(Math.random() * canvas.height);
+    }
+
+    // Star
+    function Star(x, y) {
+        this.x = x;
+        this.y = y;
+        this.radius = Math.floor(Math.random() * 2) + 1;
+        this.color = `crimson`;
+    }
+    // Ve 
+    Star.prototype.draw = function () {
+        c.fillStyle = this.color;
+        c.shadowBlur = this.r * 2;
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.closePath();
+        c.fill();
+    }
+
+    // move
+    Star.prototype.move = function () {
+        if (this.y > 0) {
+            this.y -= 1.5;
+        } else {
+            this.y = canvas.height;
+        }
+        this.draw();
+    }
+
+    let arrayStar;
+    function init() {
+        arrayStar = [];
+        for (let i = 0; i < 200; i++) {
+            arrayStar.push(new Star(createX(), createY()));
+        }
+    }
+
+    function animate() {
+        window.requestAnimationFrame(animate);
+        c.clearRect(0, 0, canvas.width, canvas.height);
+
+        arrayStar.forEach(c => {
+            c.move();
+        });
+    }
+    init();
+    animate();
+}, false);
